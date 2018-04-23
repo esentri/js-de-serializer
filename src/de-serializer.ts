f044a59edf187f18166a71d7cdd45fe33fa93059
Maurice Müller <maurice.mueller@esentri.com>
2018-04-23 16:46:57 +0200

    added serializer for export

--- src/de-serializer.ts
+++ src/de-serializer.ts
@@ -1,4 +1,5 @@
 export {Serializable} from './Serializable'
+export {Serialize, SimpleSerialize} from './Serialize'
 export {Deserialize, SimpleDeserialize} from './Deserialize'
 export {Deserializer, SimpleDeserializer} from './Deserializer'
 
