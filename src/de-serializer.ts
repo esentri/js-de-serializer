879a633bae27350d040545283dd2147d148244a6
Maurice Müller <maurice.mueller@esentri.com>
2018-06-11 17:35:22 +0200

    added DeSerializeParameterBuilder to exports

--- src/de-serializer.ts
+++ src/de-serializer.ts
@@ -3,5 +3,4 @@ export {Serialize, SimpleSerialize} from './Serialize'
 export {Deserialize, SimpleDeserialize} from './Deserialize'
 export {Deserializer, SimpleDeserializer} from './Deserializer'
 export {SerializedType} from './SerializedType'
-export {DeSerializeParameter} from './DeSerializeParameter'
-
+export {DeSerializeParameter, DeSerializeParameterBuilder} from './DeSerializeParameter'
